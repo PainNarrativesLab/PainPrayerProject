e1eec715c33c3143b2e29ba4ce06af2eea42e44e
AdamSwenson <adam.swenson@csun.edu>
2015-06-16 18:23:16 -0700

    Started adding environmental values and composer files that will be needed

--- src/lib/filemaster.php
+++ src/lib/filemaster.php
@@ -6,13 +6,31 @@
  * Date: 5/24/15
  * Time: 3:00 PM
  */
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+
+setenv("PRAY_SRC_PATH=/Users/adam/Dropbox/painprayer/src");
+setenv("PRAY_VENDOR_PATH=/Users/adam/Dropbox/painprayer/vendor");
+
+setenv("PRAY_DB_NAME=painprayer");
+setenv("PRAY_USERNAME=testuser3");
+setenv("PRAY_PASSWORD=testpass3");
+
+setenv("PRAY_RUNTYPE=normal");
+
+$src = getenv("PRAY_SRC_PATH");
+$template_cache = getenv("PRAY_TEMPLATE_CACHE");
+$template_folder = getenv("PRAY_TEMPLATE_FOLDER");
+$runtype = getenv("PRAY_RUNTYPE");
+
+
+
 //autoloader
 require_once __DIR__.'/vendor/autoload.php';
 // setup Propel
 require_once '/generated-conf/config.php';
-
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 $defaultLogger = new Logger('defaultLogger');
 $defaultLogger->pushHandler(new StreamHandler('/var/log/propel.log', Logger::WARNING));
